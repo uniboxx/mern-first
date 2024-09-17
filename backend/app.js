b952efca2c758a52d9e01125d7a4337ccd96588f
uniboxx <unibox@duck.com>
2024-09-18 00:33:43 +0200

    change feurl

--- backend/app.js
+++ backend/app.js
@@ -4,16 +4,12 @@ import 'dotenv/config';
 
 const app = express();
 const port = 8000;
-const FEURL = 'https://mern-first-frontend.onrender.com/';
+const FEURL = 'https://mern-first-frontend.onrender.com';
 const BEURL = 'http://localhost:8000';
 const corsOptions = { origin: FEURL };
 
 app.use(express.json());
 app.use(cors());
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', FEURL);
-//   next();
-// });
 
 app.get('/', (req, res) => {
   res.json({ message: 'hello world' });
