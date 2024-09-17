7e1f7e4774df7fc380c1e98c609f8ddbfa34c257
uniboxx <unibox@duck.com>
2024-09-18 00:07:51 +0200

    add cors header

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,14 @@ const corsOptions = { origin: FEURL };
 
 app.use(express.json());
 app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', FEURL); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.get('/', (req, res) => {
   res.json({ message: 'hello world' });
