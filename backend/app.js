00a38aa5f055120ec842984b747f2fe817638599
uniboxx <unibox@duck.com>
2024-09-18 04:12:51 +0200

    add lowdb

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,18 @@ import cors from 'cors';
 import 'dotenv/config';
 import { JSONFilePreset } from 'lowdb/node';
 
-const defaultData = { posts: [] };
+const defaultData = { todos: [] };
 const db = await JSONFilePreset('db.json', defaultData);
+const { todos } = db.data;
+
+// await db.update(({ posts }) => posts.push('hello world'));
+console.log(todos);
 const NODEJS_ENV = process.env.NODEJS_ENV;
 console.log(NODEJS_ENV);
 
 const app = express();
 const port = 8000;
+
 //^ attenzione l'url non deve finire con lo slash
 const FEURL =
   NODEJS_ENV === 'production'
@@ -19,12 +24,23 @@ const BEURL = 'http://localhost:8000';
 const corsOptions = { origin: FEURL };
 
 app.use(express.json());
+app.use(express.text());
 app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.json({ message: 'hello world' });
 });
 
+app.get('/todos', (req, res) => {
+  // console.log(posts);
+  res.json({ todos });
+});
+
+app.post('/todos', async (req, res) => {
+  // console.log(req.body);
+  await db.update(({ todos }) => todos.unshift(req.body));
+});
+
 app.listen(port, () => {
-  console.log(`Server listening on ${BEURL}`);
+  NODEJS_ENV === 'development' && console.log(`Server listening on ${BEURL}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
 
 import './App.css';
 
@@ -12,6 +13,30 @@ const BEURL =
 
 function App() {
   const [message, setMessage] = useState('');
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const id = nanoid();
+    // console.log(id);
+    const newTodo = { id, text };
+    // console.log(newTodo);
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(newTodo),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    };
+    fetch(BEURL + '/todos', options);
+    // .then(res => console.log(res))
+    // .then(data => console.log(data))
+    // .catch(err => console.error(err));
+
+    setTodos(prev => [newTodo, ...prev]);
+    setText('');
+  }
 
   useEffect(() => {
     fetch(BEURL)
@@ -19,9 +44,35 @@ function App() {
       .then(data => setMessage(data.message));
   }, [message]);
 
+  useEffect(() => {
+    fetch(BEURL + '/todos')
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data);
+        setTodos(data.todos);
+      })
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <>
       <h1>{message}</h1>
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label htmlFor="todo">Todo</label>
+          <input
+            type="text"
+            id="todo"
+            name="todo"
+            value={text}
+            onChange={e => setText(e.target.value)}
+          />
+        </p>
+      </form>
+      <ul>
+        {todos.length > 0 &&
+          todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+      </ul>
     </>
   );
 }
